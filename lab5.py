4085c4096eaa3b780f6c6343e601a998ea607a62
Stakhmich Karina <karinastahmic@gmail.com>
2025-11-14 11:21:25 +0200

    111

---
+++ lab5.py
@@ -0,0 +1,42 @@
+print("Hello World")
+#(!_!)(I'm shocked)
+print("Hello")
+print("World")
+#print says Hello World :)
+
+print(type("Hello World"))
+
+print(type(1.1))
+
+num = int("1")
+print(num)                 
+print(bool(num))
+
+x = 3 + 2 * 2
+print(x) 
+
+y = (3 + 2) * 2
+print(y)
+
+z = x + y
+print(z) 
+
+Letters = "ABCDEFGHIJK"
+print(Letters[:4])
+
+Good = "GsoAo+d"
+print(Good[::2]) 
+
+print("\\")
+
+word = "uppercase"
+print(word.upper())
+
+G = ("Alice went to the park together. Alice brought a book, while Bob carried a football.\
+They both enjoyed the sunny weather and shared their stories.\
+Bob knew about this book. Alice was happy.")
+print("Story: ", G)
+print(G.find("book"))
+
+G = G.replace("Alice", "Bob")
+print("\nAfter replacement:", G)
